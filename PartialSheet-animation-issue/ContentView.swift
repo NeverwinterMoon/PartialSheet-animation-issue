1d99d83946c204f28ffbcc117dc98b38d1ab9a71
Pavel Sorokin <pavel@amplerbikes.com>
2020-06-04 09:41:29 +0300

    Reproduce the issue where PartialSheet applies animation to content

--- PartialSheet-animation-issue/ContentView.swift
+++ PartialSheet-animation-issue/ContentView.swift
@@ -6,16 +6,69 @@
 //  Copyright © 2020 NeverwinterMoon. All rights reserved.
 //
 
+import PartialSheet
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @EnvironmentObject var partialSheet: PartialSheetManager
+  
+  var body: some View {
+    VStack {
+      Button(
+        action: {
+          self.partialSheet.showPartialSheet {
+            PartialSheetView()
+          }
+        },
+        label: {
+          Text("Show Partial Sheet")
+        }
+      )
     }
+    .addPartialSheet()
+  }
+}
+
+struct PartialSheetView: View {
+  var body: some View {
+    content
+  }
+  
+  private var content: some View {
+    VStack(alignment: .leading, spacing: 0) {
+      ForEach(0 ..< 4) { _ in
+        Row()
+      }
+    }
+  }
+}
+
+struct Row: View {
+  @State private var isLoading: Bool = true
+
+  var body: some View {
+    ZStack { content }
+      .onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+          self.isLoading = false
+        }
+    }
+  }
+  
+  @ViewBuilder private var content: some View {
+    if !isLoading {
+      ZStack {
+        Color.yellow
+
+        Text("Loaded view")
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+      .environmentObject(PartialSheetManager())
+  }
 }
